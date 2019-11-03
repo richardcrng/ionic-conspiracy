57e6615061d54a3b7358ba02a1ee884a673901b0
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-03 12:32:51 +0000

    Test Role for being in a conspiracy against a player

--- src/components/templates/Role/Role.test.tsx
+++ src/components/templates/Role/Role.test.tsx
@@ -94,3 +94,50 @@ describe("Reveals and hides role for an innocent player", () => {
     })
   })
 })
+
+describe("Reveals and hides role for a conspiracist", () => {
+  describe('GIVEN that Role is passed a string of a player name', () => {
+    let { getByText, getByRole, container, asFragment } = render(<Role inAConspiracyAgainst='Richard' />)
+    beforeEach(() => {
+      ({ getByText, getByRole, container, asFragment } = render(<Role inAConspiracyAgainst='Richard' />))
+    })
+
+    it("THEN the player's role is not yet revealed", () => {
+      expect(container).not.toHaveTextContent(/conspiracy/i)
+      expect(container).not.toHaveTextContent(/richard/i)
+      expect(container).not.toHaveTextContent(/innocent/i)
+    })
+
+    it("AND there is 'reveal role' text shown", () => {
+      expect(container).toHaveTextContent(/reveal role/i)
+    })
+
+    describe("WHEN the text 'reveal role' is clicked on", () => {
+      beforeEach(() => {
+        fireEvent.click(getByText(/reveal role/i))
+      })
+
+      it("THEN the player is revealed as being in a conspiracy against that player", () => {
+        expect(container).toHaveTextContent(/conspiracy/i)
+        expect(container).toHaveTextContent(/richard/i)
+        expect(container).not.toHaveTextContent(/innocent/i)
+      })
+
+      it("AND there is 'hide role' text shown", () => {
+        expect(container).toHaveTextContent(/hide role/i)
+      })
+
+      describe("AND the text 'hide role' is clicked on", () => {
+        beforeEach(() => {
+          fireEvent.click(getByText(/hide role/i))
+        })
+
+        it("THEN the player's role is hidden again", () => {
+          expect(container).not.toHaveTextContent(/conspiracy/i)
+          expect(container).not.toHaveTextContent(/richard/i)
+          expect(container).not.toHaveTextContent(/innocent/i)
+        })
+      })
+    })
+  })
+})
