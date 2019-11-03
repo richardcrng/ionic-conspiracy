5ca2d23ecbdcba70da4cedfc7a111bf3a54ca96e
richardcrng@gmail.com <richardcrng@gmail.com>
2019-11-03 12:29:41 +0000

    Test for undefined value on isInAConspiracyAgainst

--- src/components/templates/Role/Role.test.tsx
+++ src/components/templates/Role/Role.test.tsx
@@ -51,4 +51,46 @@ describe("Reveals and hides role for an innocent player", () => {
       })
     })
   })
+
+  describe('GIVEN that Role is passed undefined as a prop for isInAConspiracyAgainst', () => {
+    let { getByText, getByRole, container, asFragment } = render(<Role inAConspiracyAgainst={undefined} />)
+    beforeEach(() => {
+      ({ getByText, getByRole, container, asFragment } = render(<Role inAConspiracyAgainst={undefined} />))
+    })
+
+    it("THEN the player's role is not yet revealed", () => {
+      expect(container).not.toHaveTextContent(/innocent/i)
+      expect(container).not.toHaveTextContent(/conspiracy/i)
+    })
+
+    it("AND there is 'reveal role' text shown", () => {
+      expect(container).toHaveTextContent(/reveal role/i)
+    })
+
+    describe("WHEN the text 'reveal role' is clicked on", () => {
+      beforeEach(() => {
+        fireEvent.click(getByText(/reveal role/i))
+      })
+
+      it("THEN the player is revealed as being innocent", () => {
+        expect(container).toHaveTextContent(/innocent/i)
+        expect(container).not.toHaveTextContent(/conspiracy/i)
+      })
+
+      it("AND there is 'hide role' text shown", () => {
+        expect(container).toHaveTextContent(/hide role/i)
+      })
+
+      describe("AND the text 'hide role' is clicked on", () => {
+        beforeEach(() => {
+          fireEvent.click(getByText(/hide role/i))
+        })
+
+        it("THEN the player's role is hidden again", () => {
+          expect(container).not.toHaveTextContent(/innocent/i)
+          expect(container).not.toHaveTextContent(/conspiracy/i)
+        })
+      })
+    })
+  })
 })
